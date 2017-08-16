8dbe9baf78eb989e159bab742fd54d1b2b8a6e35
Park jongheum <whdgmawkd@naver.com>
2017-08-16 12:00:57 +0900

    optimize files and fix some error

--- app/src/main/java/kr/hs/yii/make/eyecover/services/NotificationService.java
+++ app/src/main/java/kr/hs/yii/make/eyecover/services/NotificationService.java
@@ -2,6 +2,7 @@ package kr.hs.yii.make.eyecover.services;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
@@ -32,7 +33,7 @@ public class NotificationService extends Service {
                 .setContentText("this is test notification from services");
 
         Notification mNoti = mNotiBuilder.build();
-        mNoti.flags |= Notification.FLAG_AUTO_CANCEL;
+        mNoti.flags |= Notification.FLAG_ONGOING_EVENT;
         NotificationManager mNotiManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
         mNotiManager.notify(getResources().getInteger(R.integer.notification_id),mNoti);
     }
